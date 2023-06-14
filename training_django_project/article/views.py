64a59b6f069be07631a275999151eda7399f1258
Евгений Прутян <buivan.proger@gmail.com>
2023-06-14 16:09:21 +0300

    5. Шаблонизация

--- training_django_project/article/views.py
+++ training_django_project/article/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 
 def index(request):
-    return HttpResponse('<h1>article</h1>')
+    return render(request, 'articles/index.html', context={
+        'articles': 'articles',
+    })
